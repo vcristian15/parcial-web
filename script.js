883c556dce9af5fb244f56f09f559eed16d7d676
vcristian15 <vanegasescobarc@gmail.com>
2025-09-18 16:21:28 -0700

    Se agrega el parcial

---
+++ script.js
@@ -0,0 +1,34 @@
+const obtenerValorInput = () => {
+    let inputTexto = document.getElementById("searcher")
+    let valor = inputTexto.value
+    peticionApi(valor)
+}
+
+const peticionApi = (nombre) => {
+    const baseUrl = 'https://pokeapi.co/api/v2/'
+    const endpoint = `pokemon/${nombre}`
+    const url = `${baseUrl}${endpoint}`
+
+    axios.get(url)
+    .then(res => printData(res.data))
+    .catch(err => console.log(err))
+}
+
+const printData = (data) => {
+
+    let respuesta = document.getElementById('show-info')
+
+    respuesta.innerHTML = `
+        <div class="pokemon-box">
+
+            <div class="img">
+                <img src="${data.sprites.front_default}" alt="" class="poke-img">
+            </div>
+            <div class="poke-info">
+                <p class="name">Nombre: ${data.name}</p>
+                <p class="id">ID: ${data.id}</p>
+                <p class="weight"> Peso: ${data.weight} KG</p>
+            </div>
+        </div>
+    `
+}
